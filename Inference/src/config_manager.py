717aceef710456a7ee2957304a6b466f0140ce62
XTer <xxoy1234@outlook.com>
2024-04-14 23:07:40 +0800

    修正语言问题、错误打印问题

--- Inference/src/config_manager.py
+++ Inference/src/config_manager.py
@@ -3,6 +3,14 @@ __version__ = "2.4.2 240408"
 import os,  json
 import torch
 
+import logging
+logging.getLogger("markdown_it").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("httpcore").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+logging.getLogger("charset_normalizer").setLevel(logging.ERROR)
+logging.getLogger("torchaudio._extension").setLevel(logging.ERROR)
 
 class Inference_Config():
     def __init__(self):
--- tools/i18n/i18n.py
+++ tools/i18n/i18n.py
@@ -8,13 +8,15 @@ def load_language_list(language, locale_path="./i18n/locale"):
         language_list = json.load(f)
     return language_list
 
+from Inference.src.config_manager import inference_config
 
 class I18nAuto:
     def __init__(self, language=None, locale_path="./i18n/locale"):
         if language in ["Auto", None]:
-            language = locale.getdefaultlocale()[
-                0
-            ]  # getlocale can't identify the system's language ((None, None))
+            if inference_config.locale_language in ["Auto", None, ""]:
+                language = locale.getdefaultlocale()[0]
+            else:
+                language = inference_config.locale_language
         if not os.path.exists(os.path.join(locale_path, f"{language}.json")):
             language = "en_US"
         self.language = language
